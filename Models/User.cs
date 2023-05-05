2cc54467a7113cde8cbea928b254ad74f0c455c6
negoces <negoces@hotmail.com>
2023-05-05 18:38:16 +0800

    api change

---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+namespace DemoWeb.Models
+
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using DemoWeb.Mapping;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<UsersContext>();
+builder.Services.AddDbContext<UserContext>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
    {
@@ -25,8 +25,15 @@ app.UseSwaggerUI(c =>
        c.SwaggerEndpoint("/swagger/v1/swagger.json", "DemoWeb API v1");
    });
 
-app.MapGet("/api/users", async (UsersContext db) => await db.Users.ToListAsync());
-app.MapPost("/api/user/add", async (UsersContext db, Users user) =>
+// Routers
+app.MapGet("/api/users", async (UserContext db) => 
+    await db.Users.ToListAsync());
+app.MapGet("/api/user/get/{id}", async (long id, UserContext db) =>
+    await db.Users.FindAsync(id)
+        is User user
+            ? Results.Ok(user)
+            : Results.NotFound());
+app.MapPost("/api/user/create", async (UserContext db, User user) =>
 {
     await db.Users.AddAsync(user);
     await db.SaveChangesAsync();
