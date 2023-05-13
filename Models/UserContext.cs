467dfaab7133fdb5467051aaeebde88e21a07b56
negoces <negoces@hotmail.com>
2023-05-14 02:44:38 +0800

    change sqlite -> pgsql

--- Models/UserContext.cs
+++ Models/UserContext.cs
@@ -1,13 +1,19 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace WebAPI.Models;
 
 public class UserContext : DbContext
 {
-    public UserContext(DbContextOptions<UserContext> options)
-        : base(options)
+    private readonly IConfiguration Configuration;
+
+    public UserContext(IConfiguration configuration)
     {
+        Configuration = configuration;
     }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseNpgsql(Configuration.GetConnectionString("WebApiDatabase"));
 
     public DbSet<User> User { get; set; } = null!;
 }
--- docs.md
+++ docs.md
@@ -1,8 +1,16 @@
-## 生成
+## 生成 Controllers
 
-- 需安装生成工具 `dotnet tool install -g dotnet-aspnet-codegenerator`
+- 需安装工具 `dotnet tool install -g dotnet-aspnet-codegenerator`
 
 ```pwsh
 #dotnet-aspnet-codegenerator controller -name <ControllerName> -async -api -m <ModelsName> -dc <ContextName> -outDir Controllers
 dotnet-aspnet-codegenerator controller -name UserController -async -api -m User -dc UserContext -outDir Controllers
+```
+
+## 根据 Models 自动建表
+
+- 需安装工具 `dotnet tool install -g dotnet-ef`
+
+```pwsh
+dotnet ef database update
 ```
